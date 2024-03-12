9771e64620e9eb95f656ce8072b5d5e19919c528
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-03-13 01:58:24 +0530

    Add 1st partial for Home Page
    - Scroll trigger using GSAP
    - Image animation onScroll (Core Reveal)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./views/**/*.{html,js,ejs}"],
     theme: {
+<<<<<<< Updated upstream
       extend: {},
+=======
+      extend: {
+        colors: {
+          'footer-bg': '#323B4B',
+          'home-bg': '#E5E1DC',
+          'home-bg-light': '#E4E0DD',
+          
+        },
+      },
+>>>>>>> Stashed changes
     },
     plugins: [
         require('@tailwindcss/forms'),
