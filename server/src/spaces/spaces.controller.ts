29339457e0988a527d12fed4dc297fcfa91ad4c2
aahnik <daw@aahnik.dev>
2024-03-17 22:38:20 +0530

    add spaces controller for space quota crud enpoints and correspoding services

--- server/src/spaces/spaces.controller.ts
+++ server/src/spaces/spaces.controller.ts
@@ -1,34 +1,95 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+  Request,
+  StreamableFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { SpacesService } from './spaces.service';
-import { CreateSpaceDto } from './dto/create-space.dto';
-import { UpdateSpaceDto } from './dto/update-space.dto';
+import {
+  CreateSpaceDto,
+  CreateSpacesQuotaDto,
+} from './dto/create-spaces-quota.dto';
+import {
+  UpdateSpaceDto,
+  UpdateSpacesQuotaDto,
+} from './dto/update-spaces-quota.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiTags,
+} from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import { fileStorageRootDir, oneMB } from './constants';
+import { v4 as uuidv4 } from 'uuid';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { FileUploadResponseDto } from './dto/file-upload-response';
+import { FileUploadDto } from './dto/file-upload.dto';
+import { Roles } from 'src/auth/roles.decorator';
+import { URoles } from 'src/users/users.schema';
+import { Public } from 'src/auth/public.decorator';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+
 
 @Controller('spaces')
 export class SpacesController {
-  constructor(private readonly spacesService: SpacesService) {}
+  constructor(private readonly service: SpacesService) {}
 
-  @Post()
-  create(@Body() createSpaceDto: CreateSpaceDto) {
-    return this.spacesService.create(createSpaceDto);
+
+  /*
+   * endpoints related to spaces quota
+   */
+
+  @Public()
+  @Get('/quotas')
+  @ApiOperation({ summary: 'Get list of all space quotas availaible.' })
+  async findAllSpaceQuotas() {
+    return await this.service.findAllSpaceQuotas();
   }
 
-  @Get()
-  findAll() {
-    return this.spacesService.findAll();
+  @Public()
+  @Get('/quotas/:quotaID')
+  @ApiOperation({ summary: 'Get all details of a specific spaces quota.' })
+  async findSpacesQuota(@Param('quotaID') quotaID: string) {
+    return await this.service.findSpacesQuota(quotaID);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.spacesService.findOne(+id);
+  @Roles(URoles.superuser, URoles.admin)
+  @Post('/quotas/:quotaID')
+  @ApiOperation({ summary: 'Create a new spaces quota' })
+  createSpacesQuota(
+    @Param('quotaID') quotaID: string,
+    @Body() createSpacesQuotaDto: CreateSpacesQuotaDto,
+  ) {
+    return this.service.createSpacesQuota(quotaID, createSpacesQuotaDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSpaceDto: UpdateSpaceDto) {
-    return this.spacesService.update(+id, updateSpaceDto);
+  @Roles(URoles.superuser, URoles.admin)
+  @Patch('/quotas/:quotaID')
+  @ApiOperation({ summary: 'Update and existing spaces quota' })
+  updateSpacesQuota(
+    @Param('quotaID') quotaID: string,
+    @Body() updateSpacesQuotaDto: UpdateSpacesQuotaDto,
+  ) {
+    return this.service.updateSpacesQuota(quotaID, updateSpacesQuotaDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.spacesService.remove(+id);
+  @Roles(URoles.superuser, URoles.admin)
+  @Delete('/quotas/:quotaID')
+  @ApiOperation({ summary: 'Remove an existing spaces quota' })
+  removeSpacesQuota(@Param('quotaID') quotaID: string) {
+    return this.service.removeSpacesQuota(quotaID);
   }
 }
--- server/src/spaces/spaces.service.ts
+++ server/src/spaces/spaces.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSpaceDto } from './dto/create-space.dto';
-import { UpdateSpaceDto } from './dto/update-space.dto';
+import {
+  CreateSpaceDto,
+  CreateSpacesQuotaDto,
+} from './dto/create-spaces-quota.dto';
+import {
+  UpdateSpaceDto,
+  UpdateSpacesQuotaDto,
+} from './dto/update-spaces-quota.dto';
+import { CreateFileDto } from './dto/create-file.dto';
+import { FileObject, FileObjectDocument } from './files.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { SpaceQuotas, SpaceQuotasDocument } from './spaces.schema';
 
 @Injectable()
 export class SpacesService {
-  create(createSpaceDto: CreateSpaceDto) {
-    return 'This action adds a new space';
+  constructor(
+    @InjectModel(FileObject.name) private filesModel: Model<FileObjectDocument>,
+    @InjectModel(SpaceQuotas.name)
+    private spacesQuotaModel: Model<SpaceQuotasDocument>,
+  ) {}
+
+  async findAllSpaceQuotas() {
+    return await this.spacesQuotaModel.find().exec();
   }
 
-  findAll() {
-    return `This action returns all spaces`;
+  async findSpacesQuota(quotaID: string) {
+    return await this.spacesQuotaModel.findOne({ quotaID: quotaID });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} space`;
+  async createSpacesQuota(
+    quotaID: string,
+    createSpacesQuotaDto: CreateSpacesQuotaDto,
+  ) {
+    const createdSpacesQuota = new this.spacesQuotaModel({
+      quotaID: quotaID,
+      ...createSpacesQuotaDto,
+    });
+    return await createdSpacesQuota.save();
   }
 
-  update(id: number, updateSpaceDto: UpdateSpaceDto) {
-    return `This action updates a #${id} space`;
+  async updateSpacesQuota(
+    quotaID: string,
+    updateSpacesQuotaDto: UpdateSpacesQuotaDto,
+  ) {
+    return await this.spacesQuotaModel.findOneAndUpdate(
+      { quotaID: quotaID },
+      updateSpacesQuotaDto,
+      { upsert: true },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} space`;
+  async removeSpacesQuota(quotaID: string) {
+    return await this.spacesQuotaModel.findOneAndDelete({ quotaID: quotaID });
   }
 }
