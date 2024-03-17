5abefc1ab35431fccdb582ddec12404ce661a7c0
aahnik <daw@aahnik.dev>
2024-03-17 22:38:41 +0530

    add file object schema to spaces.schema.ts

--- server/src/spaces/spaces.schema.ts
+++ server/src/spaces/spaces.schema.ts
@@ -1,6 +1,58 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
+enum FileVisibility {
+  public = 'public',
+  shared = 'shared',
+  private = 'private',
+}
+
+@Schema()
+export class FileObject {
+  @Prop({
+    required: true,
+    index: true,
+    unique: true,
+  })
+  virtualPath: string;
+  // full path to file in user's drive
+  // example: /Projects/Node/hello.js
+
+  @Prop({ required: true, default: false })
+  isDirectory: boolean;
+
+  @Prop({
+    required: true,
+    index: true,
+  })
+  owner: string;
+
+  @Prop({ required: true })
+  created: Date;
+
+  @Prop({ required: true })
+  lastEdited: Date;
+
+  @Prop({
+    required: true,
+    enum: FileVisibility,
+    default: FileVisibility.private,
+  })
+  visibility: string;
+
+  @Prop({ required: true, default: [] })
+  viewers: string[];
+
+  @Prop({ required: true, default: [] })
+  editors: string[];
+
+  @Prop({ required: true })
+  mimeType: string;
+
+  @Prop({ required: true })
+  size: number;
+}
+
 @Schema()
 export class SpaceQuotas {
   @Prop({ required: true, index: true, unique: true })
@@ -18,6 +70,9 @@ export class SpaceQuotas {
   @Prop({ required: true })
   pricePerMo: number;
 }
+
+export type FileObjectDocument = HydratedDocument<FileObject>;
+export const FileObjectSchema = SchemaFactory.createForClass(FileObject);
 
 export type SpaceQuotasDocument = HydratedDocument<SpaceQuotas>;
 export const SpaceQuotasSchema = SchemaFactory.createForClass(SpaceQuotas);
