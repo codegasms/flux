13c7038ad9379cb5c989f7b5ca77eff6f2192d35
aahnik <daw@aahnik.dev>
2024-03-17 23:11:36 +0530

    register models with mogoose for mods and spaces

--- server/src/mods/mods.module.ts
+++ server/src/mods/mods.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ModsService } from './mods.service';
 import { ModsController } from './mods.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ModTokenPlans,
+  ModTokenPlansSchema,
+  Mods,
+  ModsSchema,
+} from './mods.schema';
 
 @Module({
   controllers: [ModsController],
   providers: [ModsService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Mods.name, schema: ModsSchema },
+      { name: ModTokenPlans.name, schema: ModTokenPlansSchema },
+    ]),
+  ],
 })
 export class ModsModule {}
--- server/src/spaces/spaces.module.ts
+++ server/src/spaces/spaces.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SpacesService } from './spaces.service';
 import { SpacesController } from './spaces.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  FileObject,
+  FileObjectSchema,
+  SpaceQuotas,
+  SpaceQuotasSchema,
+} from './spaces.schema';
 
 @Module({
   controllers: [SpacesController],
   providers: [SpacesService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: FileObject.name, schema: FileObjectSchema },
+      { name: SpaceQuotas.name, schema: SpaceQuotasSchema },
+    ]),
+  ],
 })
 export class SpacesModule {}
