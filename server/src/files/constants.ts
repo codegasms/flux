b89aae0154aa543932f4215f95eb0c39550f4a7f
aahnik <daw@aahnik.dev>
2024-03-17 20:55:17 +0530

    remove the files module (functionality to be moved under spaces)

--- server/src/files/constants.ts
+++
@@ -1,3 +0,0 @@
-export const fileStorageRootDir = './storage';
-export const oneKB = 1000;
-export const oneMB = 1000 * oneKB;
--- server/src/files/dto/file-upload-response.ts
+++
@@ -1,3 +0,0 @@
-import { CreateFileDto } from './create-file.dto';
-
-export class FileUploadResponseDto extends CreateFileDto {}
