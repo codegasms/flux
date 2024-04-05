dd8455bf4bbf6a3b7fdb7528e29cb03d4619ed88
virinci <revirinci@proton.me>
2024-04-05 17:01:44 +0530

    Minor refactor of frontend serving function

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'node:path';
 import ejs from 'ejs';
 import dotenv from 'dotenv';
 
@@ -20,40 +21,41 @@ server.engine('ejs', async (path, data, cb) => {
 });
 
 function filesToRender(dir) {
-  function fileSubstring(file, dir) {
-    return file.substring(0, file.length - 4).replace(dir, '');
-  }
+  function readDirRecursive(dir, fileList) {
+    fileList = fileList ?? [];
 
-  function readDirRecursive(dir, filelist) {
-    let files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.map((file) => {
-      if (fs.statSync(dir + '/' + file).isDirectory()) {
-        filelist = readDirRecursive(dir + '/' + file, filelist);
+    const dirents = fs.readdirSync(dir, { withFileTypes: true });
+
+    dirents.map((dirent) => {
+      const filePath = path.join(dir, dirent.name);
+      if (dirent.isDirectory()) {
+        fileList = readDirRecursive(filePath, fileList);
       } else {
-        filelist.push(dir + '/' + file);
+        fileList.push(filePath);
       }
     });
-    return filelist;
+
+    return fileList;
   }
 
-  return readDirRecursive(dir).map((file) => {
-    return fileSubstring(file, dir);
-  });
+  return readDirRecursive(dir)
+    .filter((file) => path.extname(file) === '.ejs')
+    .map((file) => file.substring(dir.length, file.length - 4));
 }
 
 function renderFile(file) {
-  server.get(file, (req, res) => {
+  server.get(file, (_req, res) => {
     return res.render(file.slice(1));
   });
+
   if (file.slice(-5) === 'index') {
-    server.get(file.substring(0, file.length - 5), (req, res) => {
+    server.get(file.substring(0, file.length - 5), (_req, res) => {
       return res.render(file.slice(1));
     });
   }
 }
 
-filesToRender('views').map((file) => renderFile(file));
+filesToRender('views').forEach(renderFile);
 
 server.use(express.static('public'));
 
