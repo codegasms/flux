5ed5f40378eeeac1024e0b48d4d44329c57486d2
aahnik <daw@aahnik.dev>
2024-04-21 16:16:24 +0530

    [backend] implement several features in spaces service
    - checks of whether directory exists
    - checks of whether file does not exist
    - fetchUsage to do mongo db aggregation
    - update findMeta to utilize new utility functions
    - implement recoverFromTrash
    - implement copyFile and moveFile

--- server/src/spaces/spaces.service.ts
+++ server/src/spaces/spaces.service.ts
@@ -1,4 +1,11 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  NotImplementedException,
+  PreconditionFailedException,
+} from '@nestjs/common';
 import { CreateSpacesQuotaDto } from './dto/create-spaces-quota.dto';
 import { UpdateSpacesQuotaDto } from './dto/update-spaces-quota.dto';
 import { CreateFileDto } from './dto/create-file.dto';
@@ -12,36 +19,90 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ShareFileDto } from './dto/share-file-dto';
 import { startsWith } from 'src/utils/regex';
-import { splitSpacePath } from 'src/utils/space-paths';
+import {
+  isSpaceRoot,
+  joinSpacePath,
+  splitSpacePath,
+  trimSlashes,
+} from 'src/utils/space-paths';
 import { SpacePath } from './dto/space-path.dto';
 import { CopyFileDto } from './dto/copy-file.dto';
 import { MoveFileDto } from './dto/move-file.dto';
+import * as path from 'path';
+import * as fs from 'node:fs/promises';
+import { fileStorageRootDir } from './constants';
+import { IFileObjectQuery } from './entities/file-object-query.interface';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class SpacesService {
   constructor(
+    private usersService: UsersService,
     @InjectModel(FileObject.name) private filesModel: Model<FileObjectDocument>,
     @InjectModel(SpaceQuotas.name)
     private spacesQuotaModel: Model<SpaceQuotasDocument>,
   ) {}
 
-  async createFile(createFileDto: CreateFileDto): Promise<FileObject> {
-    const { sParent, fileName } = splitSpacePath(createFileDto.spaceParent);
-    console.log('***** in createFile service');
-    console.log(sParent);
-    console.log(fileName);
-    if (sParent == '/' && !fileName) {
-      // creation of new stuff in root dir
-      console.log('creating stuff in root dir');
+  async assureDirExists(ownerID: string, spacePath: SpacePath): Promise<void> {
+    // assert whether the given spacePath is a valid directory in the owner's space
+    if (spacePath.spaceParent === '/' && spacePath.fileName === '') return;
+    const parentDir = await this.filesModel.findOne({
+      owner: ownerID,
+      spaceParent: spacePath.spaceParent,
+      fileName: spacePath.fileName,
+      isDir: true,
+    });
+    if (!parentDir)
+      throw new ForbiddenException(
+        `Directory ${path.join(spacePath.spaceParent, spacePath.fileName)} does not exist.`,
+      );
+  }
+
+  async assureFileDoesNotExist(
+    ownerID: string,
+    spacePath: SpacePath,
+  ): Promise<void> {
+    let isRoot = false;
+    let fileObj = null;
+    if (spacePath.spaceParent === '/' && spacePath.fileName === '') {
+      isRoot = true;
     } else {
-      const parentDir = await this.filesModel.findOne({
-        spaceParent: sParent,
-        fileName: fileName,
-        isDir: true,
+      fileObj = await this.filesModel.findOne({
+        owner: ownerID,
+        spaceParent: spacePath.spaceParent,
+        fileName: spacePath.fileName,
       });
-      if (!parentDir)
-        throw new ForbiddenException('Parent Directory does not exist');
     }
+
+    if (fileObj || isRoot)
+      throw new ForbiddenException(
+        `Object ${path.join(spacePath.spaceParent, spacePath.fileName)} already exists`,
+      );
+  }
+
+  async fetchUsage(ownerId: string, spacePath: SpacePath) {
+    const match = {
+      isDir: false,
+      inTrash: false,
+    };
+    if (!isSpaceRoot(spacePath)) {
+      match['spaceParent'] = startsWith(joinSpacePath(spacePath));
+    }
+    return await this.filesModel.aggregate([
+      {
+        $match: match,
+      },
+      {
+        $group: { _id: '$mimeType', totalSize: { $sum: '$size' } },
+      },
+    ]);
+  }
+
+  async createFile(createFileDto: CreateFileDto): Promise<FileObject> {
+    await this.assureDirExists(
+      createFileDto.owner,
+      splitSpacePath(createFileDto.spaceParent),
+    );
 
     const existingFileObject = await this.filesModel.findOne({
       spaceParent: createFileDto.spaceParent,
@@ -70,15 +131,31 @@ export class SpacesService {
     return await createdFileObject.save();
   }
 
-  async findMeta(ownerId: string, spacePath: SpacePath) {
-    const query = {
+  async findMeta(
+    ownerId: string,
+    spacePath: SpacePath,
+    inTrash: boolean = false,
+  ) {
+    const query: IFileObjectQuery = {
       owner: ownerId,
       spaceParent: spacePath.spaceParent,
-      inTrash: false,
+      inTrash: inTrash,
     };
-    if (spacePath.fileName !== '/') query['fileName'] = spacePath.fileName;
-
-    return await this.filesModel.find({ ...query }).exec();
+    // when spacePath is root, skip adding fileName to query
+    if (isSpaceRoot(spacePath)) {
+      return await this.filesModel.find({ ...query }).exec();
+    }
+    query['fileName'] = spacePath.fileName;
+    const fileObj = await this.filesModel.findOne({ ...query });
+    if (!fileObj)
+      throw new NotFoundException('No such file or directory found');
+    if (fileObj.isDir) {
+      delete query['fileName'];
+      query['spaceParent'] = startsWith(joinSpacePath(spacePath));
+      const children = await this.filesModel.find({ ...query }).exec();
+      return [fileObj, ...children];
+    }
+    return fileObj;
   }
 
   async shareFile(ownerId: string, shareFileDto: ShareFileDto) {
@@ -92,12 +169,21 @@ export class SpacesService {
     );
   }
 
+  // moveToTrash and recoverFromTrash can have potential conflicts with same file poth in complement, which complementation is caused
+  // think how to handle this
+
   async moveToTrash(ownerId: string, spacePath: SpacePath) {
     const fileObj = await this.filesModel.findOne({
       owner: ownerId,
       spaceParent: spacePath.spaceParent,
       fileName: spacePath.fileName,
+      inTrash: false,
     });
+    if (!fileObj) {
+      throw new PreconditionFailedException(
+        'No such item exists outside trash',
+      );
+    }
 
     if (fileObj.isDir) {
       const children = await this.filesModel
@@ -112,6 +198,34 @@ export class SpacesService {
       }
     } else {
       fileObj.inTrash = true;
+      await fileObj.save();
+    }
+  }
+
+  async recoverFromTrash(ownerId: string, spacePath: SpacePath) {
+    const fileObj = await this.filesModel.findOne({
+      owner: ownerId,
+      spaceParent: spacePath.spaceParent,
+      fileName: spacePath.fileName,
+      inTrash: true,
+    });
+    if (!fileObj) {
+      throw new PreconditionFailedException('No such item exists in trash');
+    }
+
+    if (fileObj.isDir) {
+      const children = await this.filesModel
+        .find({
+          owner: ownerId,
+          spaceParent: startsWith(spacePath.spaceParent),
+        })
+        .exec();
+      for (const fileObjItem of children) {
+        fileObjItem.inTrash = true;
+        await fileObjItem.save();
+      }
+    } else {
+      fileObj.inTrash = false;
       await fileObj.save();
     }
   }
@@ -138,11 +252,11 @@ export class SpacesService {
     const actualName = fileObj.fileName;
     console.log('checking parent dires for acc');
     while (fileObj.spaceParent != '/') {
-      const { sParent, fileName } = splitSpacePath(fileObj.spaceParent);
-      console.log(sParent);
+      const { spaceParent, fileName } = splitSpacePath(fileObj.spaceParent);
+      console.log(spaceParent);
       console.log(fileName);
       fileObj = await this.filesModel.findOne({
-        spaceParent: sParent,
+        spaceParent: spaceParent,
         fileName: fileName,
         isDir: true,
         inTrash: false,
@@ -157,13 +271,79 @@ export class SpacesService {
   async copyFile(ownerId: string, copyFileDto: CopyFileDto) {
     console.log(ownerId);
     console.log(copyFileDto);
-    return 'done';
+    const fileObj = await this.filesModel.findOne({
+      owner: ownerId,
+      spaceParent: copyFileDto.spaceParent,
+      fileName: copyFileDto.fileName,
+    });
+    if (fileObj.isDir) {
+      throw new NotImplementedException(
+        'Copying directories is not currently supported!',
+      );
+    }
+    this.assureDirExists(ownerId, splitSpacePath(copyFileDto.destinationDir));
+    const now = new Date();
+    const copiedFileObj = new this.filesModel({
+      spaceParent: copyFileDto.destinationDir,
+      fileName: fileObj.fileName,
+      size: fileObj.size,
+      mimeType: fileObj.mimeType,
+      created: now,
+      lastEdited: now,
+      owner: ownerId,
+    });
+
+    const cpFileObj = await copiedFileObj.save();
+    await fs.copyFile(
+      path.join(fileStorageRootDir, fileObj.id),
+      path.join(fileStorageRootDir, cpFileObj.id),
+    );
+    await this.usersService.consumeStorageSpace(ownerId, fileObj.size);
+    return cpFileObj;
   }
 
   async moveFile(ownerId: string, moveFileDto: MoveFileDto) {
     console.log(ownerId);
     console.log(moveFileDto);
-    return 'done';
+    const fileObj = await this.filesModel.findOne({
+      owner: ownerId,
+      spaceParent: moveFileDto.sourcePath.spaceParent,
+      fileName: moveFileDto.sourcePath.fileName,
+    });
+    await this.assureDirExists(
+      ownerId,
+      splitSpacePath(moveFileDto.destPath.spaceParent),
+    );
+    await this.assureFileDoesNotExist(ownerId, moveFileDto.destPath);
+    // the fileObj will be moved under new parent folder
+    // moveFileDto.destPath.spaceParent
+    // as name
+    // moveFileDto.destPath.fileName
+    if (fileObj.isDir) {
+      console.log('to move is a dir');
+      const oldParentPath = trimSlashes(joinSpacePath(moveFileDto.sourcePath));
+      const newParentPath = trimSlashes(joinSpacePath(moveFileDto.destPath));
+      console.log(oldParentPath);
+      console.log(newParentPath);
+      const children = await this.filesModel
+        .find({
+          owner: ownerId,
+          spaceParent: startsWith(oldParentPath),
+        })
+        .exec();
+      console.log('children');
+      for (const fileObjItem of children) {
+        console.log(fileObjItem.fileName);
+        fileObjItem.spaceParent = fileObjItem.spaceParent.replace(
+          oldParentPath,
+          newParentPath,
+        );
+        await fileObjItem.save();
+      }
+    }
+    fileObj.spaceParent = moveFileDto.destPath.spaceParent;
+    fileObj.fileName = moveFileDto.destPath.fileName;
+    return await fileObj.save();
   }
 
   async findAllSpaceQuotas() {
