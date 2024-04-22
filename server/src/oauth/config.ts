9b8cb3f5f0731ef5805baf4b826aa7b195abad50
aahnik <daw@aahnik.dev>
2024-04-22 23:29:15 +0530

    [backend] in oauth config, add frontend url, which will be used for redirects after succesful auth

--- server/src/oauth/config.ts
+++ server/src/oauth/config.ts
@@ -2,6 +2,7 @@ import fromEnv from 'src/utils/env';
 import { configDotenv } from 'dotenv';
 configDotenv();
 export const oauthConfig = {
+  frontendUrl: fromEnv('FRONTEND_URL', 'http://localhost:3000/docs'),
   google: {
     clientID: fromEnv('GOOGLE_OAUTH_CLIENT_ID'),
     clientSecret: fromEnv('GOOGLE_OAUTH_CLIENT_SECRET'),
