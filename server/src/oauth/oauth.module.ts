a6d23b0ff90a9dabd6bc0207dbf742550f733639
aahnik <daw@aahnik.dev>
2024-04-22 19:53:55 +0530

    [backend] register AuthModule as a dependancy for oauth module

--- server/src/oauth/oauth.module.ts
+++ server/src/oauth/oauth.module.ts
@@ -3,8 +3,11 @@ import { OauthService } from './oauth.service';
 import { OauthController } from './oauth.controller';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { GitHubStrategy } from './strategies/github.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [UsersModule, AuthModule],
   controllers: [OauthController],
   providers: [OauthService, GoogleStrategy, GitHubStrategy],
 })
