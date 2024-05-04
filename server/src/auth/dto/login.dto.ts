da2f5019267144aa58cef62b85199e70c8fd4504
aahnik <daw@aahnik.dev>
2024-05-04 23:58:43 +0530

    [backend] add some default values for auth dtos for swagger ui

--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SingInDto {
   @IsEmail()
+  @ApiProperty({ default: 'daw@aahnik.dev' })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({ default: 'hello void' })
   password: string;
 }
--- server/src/auth/dto/register.dto.ts
+++ server/src/auth/dto/register.dto.ts
@@ -1,12 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
+  @ApiProperty({ default: 'daw@aahnik.dev' })
   email: string;
 
+  @IsNotEmpty()
+  @ApiProperty({ default: 'Void' })
   fullName: string;
 
   @Length(8, 72)
+  @ApiProperty({ default: 'hello void' })
   @IsNotEmpty()
   password: string;
 }
